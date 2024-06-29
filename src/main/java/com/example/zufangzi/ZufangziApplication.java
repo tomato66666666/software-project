5a2387ab266d354e478ab5adb9611aeb3d88d433
黄一帆 <1341595563@qq.com>
2024-06-29 16:17:27 +0800

    Initial commit

---
+++ src/main/java/com/example/zufangzi/ZufangziApplication.java
@@ -0,0 +1,12 @@
+package com.example.zufangzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZufangziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZufangziApplication.class, args);
+    }
+}
